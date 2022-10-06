f9e6572675b0ed2b9d23117c1f2a622ad9c11940
douglastorres82 <d.luistorres@gmail.com>
2022-10-06 09:10:09 -0400

    feat: add login page and form

---
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export const Dashboard = () => {
+
+  return (
+    <div>
+        <p> Dashborad </p>
+        <Link to="/entrar"> Login </Link>
+    </div>
+
+  )
+}
---
+++ src/app/pages/login/Login.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [senha, setPassword] = useState('');
+
+    const handleEntrar = () => {
+        console.log(email);
+        console.log(senha);
+    }
+
+    return(
+        <div>
+            <form>
+                
+                <label >
+                    <span>E-mail</span>
+                    <input  value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+
+                <label >
+                    <span>Senha</span>
+                    <input  type="password" value={senha} onChange={e => setPassword(e.target.value)}/>
+                </label>
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>    
+            
+        </div>
+    );
+}
